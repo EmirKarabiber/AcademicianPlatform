49e926747352d8c8d2d4e3defa527231df872df4
devmehmetakifv <go4webb@gmail.com>
2023-08-29 12:31:16 +0300

    AWS SQL Server has been installed. From now on, all developers working on the project can work in the same database.

---
+++ AcademicianPlatformLoginPrototype/Data/Migrations/20230829092220_PublicDBMigration_AKADEMISTUN.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademicianPlatform.Data.Migrations
+{
+    public partial class PublicDBMigration_AKADEMISTUN : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
