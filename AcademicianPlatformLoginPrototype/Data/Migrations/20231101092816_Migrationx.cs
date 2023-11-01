9b9222055f76cb141192c11b42fa4b9d201148cd
Esforper <dolanbayomerfaruk@gmail.com>
2023-11-01 12:46:52 +0300

    major fix

---
+++ AcademicianPlatformLoginPrototype/Data/Migrations/20231101092816_Migrationx.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademicianPlatform.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrationx : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AnnouncementSenderId",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnnouncementSenderId",
+                table: "Comments");
+        }
+    }
+}
