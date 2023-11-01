6b361744f35ea20b2e988191f97418c569e4aa22
devmehmetakifv <go4webb@gmail.com>
2023-11-01 10:55:58 +0300

    TinyMCE Free Version Rich Text Editor has been installed to NewAnnouncement page. Responsiveness should be increased.

--- AcademicianPlatformLoginPrototype/wwwroot/js/site.js
+++ AcademicianPlatformLoginPrototype/wwwroot/js/site.js
@@ -1,4 +1,18 @@
-﻿//Announcement edit code
+﻿//TinyMCE
+tinymce.init({
+    selector: 'textarea#AnnouncementContent',
+    plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+        { value: 'First.Name', title: 'First Name' },
+        { value: 'Email', title: 'Email' },
+    ],
+    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+});
+
+//Announcement edit code
 $('.edit-btn').click(function () {
     var announcementId = $(this).data('id');
     var announcementTitle = $(this).data('title');
